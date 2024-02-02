1160b489f2d20172ba922fcb176281726546a17c
abhijitmoe <146827748+abhijitmoe@users.noreply.github.com>
2024-02-02 13:54:12 +0530

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,2 +1,15 @@
 importScripts("https://cdn.moengage.com/webpush/releases/serviceworker_cdn.min.latest.js");
 importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
+
+AMP_SW.init({
+  assetCachingOptions: [
+    {
+      regexp: /\.(png|jpg|woff2|woff|css|js)/,
+      cachingStrategy: 'CACHE_FIRST' // options are NETWORK_FIRST | CACHE_FIRST | STALE_WHILE_REVALIDATE
+    }
+  ],
+  offlinePageOptions: {
+    url: '/offline',
+    assets: []
+  }
+});
