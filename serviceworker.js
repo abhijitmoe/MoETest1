813630a3a2e823d5565233849f6cda52b4886a6f
abhijitmoe <146827748+abhijitmoe@users.noreply.github.com>
2024-02-02 13:51:44 +0530

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,2 +1,2 @@
-//importScripts("https://cdn.moengage.com/webpush/releases/serviceworker_cdn.min.latest.js");
+importScripts("https://cdn.moengage.com/webpush/releases/serviceworker_cdn.min.latest.js");
 importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
